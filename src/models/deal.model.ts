4f254136119e27a3d787d9a389fdda83e7d27d46
murtaza335 <murtazarasheedblogger@gmail.com>
2026-04-30 01:18:03 +0500

    resolved some build errors

--- src/models/deal.model.ts
+++ src/models/deal.model.ts
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 
 //our schema for the deal data that we will be storing in our database
 export interface DealDocument extends Document {
+  _id: Types.ObjectId;
   dealId: string; // UUID string
   brandId: Types.ObjectId; // Reference to Brand
   brandSlug: string; // denormalized for easier querying without needing to populate brand
@@ -19,7 +20,7 @@ export interface DealDocument extends Document {
   mealType?: string[];
   conditions?: string;
   startTime?: Date;
-  endTime: Date;
+  endTime?: Date;
   isExpired: boolean;
   isActive: boolean;
   isHot: boolean;
@@ -53,8 +54,8 @@ const dealSchema = new Schema<DealDocument>(
     cuisineTags: [{ type: String }],
     mealType: [{ type: String }],
     conditions: { type: String },
-    startTime: { type: Date , nullable: true},
-    endTime: { type: Date , nullable: true},
+    startTime: { type: Date },
+    endTime: { type: Date},
     isExpired: {
       type: Boolean,
       default: false,
@@ -76,4 +77,8 @@ const dealSchema = new Schema<DealDocument>(
 
 dealSchema.index({ brandId: 1, externalId: 1 }, { unique: true });
 
-export const DealModel = mongoose.model("Deal", dealSchema, "deals");
+export const DealModel = mongoose.model<DealDocument>(
+  "Deal",
+  dealSchema,
+  "deals"
+);
